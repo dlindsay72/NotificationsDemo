0533059299d4b858c0d1de592f0a66390e98a39b
Dan Lindsay <dlindsay72@hotmail.com>
2017-02-23 13:08:47 -0500

    scheduleNotification function created and working

--- NotificationsDemo/ViewController.swift
+++ NotificationsDemo/ViewController.swift
@@ -7,18 +7,37 @@
 //
 
 import UIKit
+import UserNotifications
 
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        scheduleNotification()
+    }
+    
+    func scheduleNotification() {
+        let content = UNMutableNotificationContent()
+        content.title = "10 second notification demo"
+        content.subtitle = "From Treehouse"
+        content.body = "Notification after 10 seconds - Your alert is ready!"
+        content.badge = 1
+        content.sound = UNNotificationSound(named: "gong.aif")
+        
+        
+        let imageURL = Bundle.main.url(forResource: "treehouse", withExtension: "jpg")
+        let attachment = try! UNNotificationAttachment(identifier: "treehouse.jpg", url: imageURL!, options: nil)
+        content.attachments = [attachment]
+        
+        let trigger = UNTimeIntervalNotificationTrigger(timeInterval: 10.0, repeats: false)
+        let request = UNNotificationRequest(identifier: "10.second.notification", content: content, trigger: trigger)
+        
+        UNUserNotificationCenter.current().add(request, withCompletionHandler: nil)
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
+   
+    
 
 
 }
